38c64cce20f0baa253a5cdaa015190f9c22f371d
javiern520 <94005015+javiern520@users.noreply.github.com>
2022-08-31 10:41:12 +0200

    Add files via upload
    update of the script, addition of second mode of the game in which the computer can guess the number

--- 07_guess_game/main.py
+++ 07_guess_game/main.py
@@ -18,4 +18,30 @@ def guess(x):
             print('Guess is too low!!')
     print(f'You guessed the number {num} correctly!!!')
 
-guess(10)
+def pc_guess(x):
+    print(f'Please choose a number between 1 and {x}')
+    key=''
+    while key != 'yes':
+        key=input('Are you ready? \n').lower()
+    num = ''
+    low = 1
+    high = x
+    while num != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        print(f'The number is {guess}')
+        num = input('Is the number correct (C), too high (H) or too low(L)? \n').lower()
+        if num == 'h':
+            high = guess-1
+        elif num == 'l':
+            low = guess+1         
+    print(f'Your number is {guess}')
+
+print('How do you want to play?')
+mode = input('Should the computer(pc) guess or should you (me) guess?\n').lower()
+if mode == 'me':
+    guess(10)
+else:
+    pc_guess(10)
